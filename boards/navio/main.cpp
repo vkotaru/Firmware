cb0616da7515ef68b2e29525f3c70ed70ece2556
vkotaru <17078723+vkotaru@users.noreply.github.com>
2020-02-22 11:21:50 -0800

    work-in-progress: compile roflight firmware with navio

--- boards/navio/main.cpp
+++ boards/navio/main.cpp
@@ -29,7 +29,7 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-// #include "navio_board.h"
+#include "navio_board.h"
 // #include "rosflight.h"
 // #include "mavlink.h"
 #include <stdio.h>
